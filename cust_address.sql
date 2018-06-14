b883dc6d469d5132cc2e986c73104eef5c564b78
Sam Ogunyowa <mailsamog@gmail.com>
2018-06-14 12:04:13 -0400

    delete some files

--- cust_address.sql
+++
@@ -1,5 +0,0 @@
-h¥
-×.Fa(¤ÙÇíe²w¶†µ˜q+Ú@Yµš[„²‚÷©	©ÍÉMTºõò«5¼ÑFîøH³Ð—H‚›bŽŽEÇ¢HÊ´
-o—t•™ÜË¶Žt*ŠPæÖoÛs,[k†'··Ç¶
-UD_·çÒh¤¢¹R¢òŒ'4¢#L–«¬¶¤Ü\¶¯Íé=-6ÐªÄôÃ=X2%EœËÔ³øhAºœ
-"† £n¨;e”¶8IÚO«F#–Ôõ¸ULy^Ó$8£‡$¹\ÆÁ$‚ÂÑØb?+2ŠbmB‘â	_?S¤èo'ÁFü
--- samog_work.sql
+++
@@ -1,2 +0,0 @@
-Ô|ÚnL_9Þÿ†ä%[Ww´iÄE¼žk/¶ËÂ¬º¹±P`è­÷ÚÒX3$N'L–D×¡ÒÎÁ­Z?=Î±¼Iå¤+µAØ@½W^û%¼)ßä Õ„½$Wé‰¢aµ-ˆD%Iè¨âÉ„m1»h­"†žETû"Z’ª˜ 5íõý°gèž?˜ã9â}ø0a“\ãa–Ö°Z£ãÛÌjZ¼m÷ÇNw&‰ËÝ%'}<c·–‹^¤¢XÊ
-p|2(zN*N½M^@tÚe+F3µ•|3J S®¥
